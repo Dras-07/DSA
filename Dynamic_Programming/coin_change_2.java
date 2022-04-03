19c9719691d26bc1479c15de1d49d63e90fb60ad
Anjanay Singh <63287310+Dras-07@users.noreply.github.com>
2022-04-03 17:30:56 +0530

    Create coin_change_2.java

---
+++ Dynamic_Programming/coin_change_2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        
+        int dp[]=new int[amount+1];
+        dp[0]=1;
+        for(int i:coins)
+        {
+            for(int j=i;j<=amount;j++)
+            {
+               
+                    dp[j]+=dp[j-i];
+                
+            
+            }
+        }
+        return dp[amount];
+    }
+}
